110f38f9bf68a585eccc4e565b13a6dfb005224a
archit-soni <43818375+archit-soni@users.noreply.github.com>
2020-07-19 13:35:49 -0600

    posts

--- app1/models.py
+++ app1/models.py
@@ -1,24 +1,18 @@
+import datetime
+from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
+from slugify import slugify
 
 # Create your models here.
 
-STATUS = (
-    (0,"Draft"),
-    (1,"Publish")
-)
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    location = models.CharField(max_length=200, null=True, blank=True)
-    content = models.TextField()
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now= True)
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    user= models.ForeignKey(User, on_delete=models.DO_NOTHING, default=1)
+    title= models.CharField(max_length=500)
+    content= models.TextField()
+    url= models.SlugField(max_length=500, unique=False, blank=True, editable=False)
 
-    class Meta:
-        ordering = ['-created_on']
-
-    def __str__(self):
-        return self.title
+    def save(self, *args, **kwargs):
+        self.url= self.title
+        super(Post, self).save(*args, **kwargs)
--- app1/views.py
+++ app1/views.py
@@ -2,7 +2,54 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .forms import PostForm
+from django.http import HttpResponseRedirect
+
+
+#create view
+def posts_create_view(request):
+    form= PostForm(request.POST or None)
+    
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect("/posts/")
+
+    
+    
+    context= {'form': form,
+              }
+    
+    return render(request, 'posts-create-view.html', context)
+
+
+
+#list view
+def posts_list_view(request):
+
+    allposts= Post.objects.all()
+    
+    context= {'allposts': allposts,
+              }
+    
+    return render(request, 'posts-list-view.html', context)
+
+
+#detail view
+def posts_detail_view(request, url=None):
+
+    post= get_object_or_404(Post, url=url)
+
+
+    
+    
+    context= {'post': post,
+              }
+    
+    return render(request, 'posts-detail-view.html', context)
 
 def index(request):
     return render(request,'index.html')
@@ -17,7 +64,7 @@ def sign_up(request):
     context['form']=form
     return render(request,'registration/signup.html',context)
 #@login_required
-#def index(request):
+#def index2(request):
 #    return render(request, 'index2.html')
 
 
