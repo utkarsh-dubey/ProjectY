c885b6bac13120476d450dbb5210ce5073912188
archit-soni <43818375+archit-soni@users.noreply.github.com>
2020-07-19 04:19:59 -0600

    login signup

--- ProjectY/urls.py
+++ ProjectY/urls.py
@@ -1,11 +1,9 @@
 
-
 from django.contrib import admin
-from django.urls import path
-from django.urls import include,path
-
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("app1.urls")),
+    path('accounts/',include('django.contrib.auth.urls'))
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,23 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 def index(request):
-    a=0
-    return render(request,"index.html")
+    return render(request,'index.html')
+def sign_up(request):
+    context = {}
+    form = UserCreationForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.save()
+            login(request,user)
+            return render(request,'accounts/index2.html')
+    context['form']=form
+    return render(request,'registration/signup.html',context)
+#@login_required
+#def index(request):
+#    return render(request, 'index2.html')
+
+
