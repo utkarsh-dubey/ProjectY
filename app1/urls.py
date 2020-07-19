2786e80ecc5a01161e83b0ca1487d05ee76d9e67
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2020-07-19 15:55:12 +0530

    ok

--- app1/urls.py
+++ app1/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
 from . import views
 
-urlpatterns=[
-	path('',views.index, name='index'),
+urlpatterns = [
+    path('',views.index,name="home"),
+    path('accounts/signup/',views.sign_up,name="sign-up")
 ]
