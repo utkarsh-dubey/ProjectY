24f2ce2158de455d58551f52cbf54e6a0b91b2e5
archit-soni <43818375+archit-soni@users.noreply.github.com>
2020-07-21 00:59:01 -0600

    fixed nba title, added author name and category in post details

--- app1/models.py
+++ app1/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.db import models
 from django.contrib.auth.models import User
 from slugify import slugify
+import random
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -12,7 +13,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
     bio = models.TextField(max_length=500, blank=True)
-    avatar = models.ImageField(null=True)
     location = models.CharField(max_length=30, blank=True, null=True)
     birth_date = models.DateField(null=True, blank=True)
     first_name = models.CharField(max_length=30, blank=False, default='First')
