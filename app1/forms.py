3dec258dfac33f5f3c90d959f9ab54fdaa2ba2be
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2020-07-22 02:39:06 +0530

    Revert "comments op"
    This reverts commit fc5aa807b66e1d25311b0352e5371cdd01314d78.

--- app1/forms.py
+++ app1/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Post,Comment
+from .models import Post
 from django.core.files.images import get_image_dimensions
 from django import forms
 from app1.models import Profile
@@ -35,10 +35,4 @@ class UserForm(forms.ModelForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model= Post
-        fields= ["title", "content", "category","user"]
-
-class CommentForm(forms.ModelForm):
-
-    class Meta:
-        model = Comment
-        fields = ('name', 'body')
+        fields= ["title", "content", "category"]
