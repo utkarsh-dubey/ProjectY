f9d9b1643c8536f38760f475879d192c68aeaeda
archit-soni <43818375+archit-soni@users.noreply.github.com>
2020-07-21 01:46:41 -0600

    redirect homepage

--- app1/views.py
+++ app1/views.py
@@ -244,7 +244,7 @@ def login_view(request):
         if(form.is_valid()):
             user=form.get_user()
             login(request,user)
-            return render(request,'')
+            return render(request,'index.html')
 
         else:
             form=AuthenticationForm()
