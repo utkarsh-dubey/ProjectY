fad2d8dcfc2a89cb1ef0fbce41a5d44429fdd8be
archit-soni <43818375+archit-soni@users.noreply.github.com>
2020-07-21 03:11:05 -0600

    Update models.py

--- app1/models.py
+++ app1/models.py
@@ -26,10 +26,10 @@ class Profile(models.Model):
     def save(self):
         super().save()
 
-        img = image.open(self.image.path)
+        img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
-            output.size = (300,300)
+            output_size = (300,300)
             img.thumbnail(output_size)
             img.save(self.image.path)
 
