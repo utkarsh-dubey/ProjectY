fc5e129c462421722cd11803bb0be83ee40d601a
satvikkaul <satvikkaul123@gmail.com>
2020-07-21 01:10:39 -0400

    profile stuff

--- ProjectY/urls.py
+++ ProjectY/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView, LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +14,4 @@ urlpatterns = [
     path('accounts/logout/', view = LogoutView.as_view(), name='logout', kwargs={'next_page' : '/'}),
 #    auth_views.LogoutView.as_view(template_name = 'logout.html')
 
-]
+] + static(settings.MEDIA_URL, docoment_root=settings.MEDIA_ROOT)#ONLY TO BE USED IN DEBUG MODE
