fa4b7c358707d66ed3d57271aaaed3dbc083f0c7
Utkarsh dubey <dubeyutkarsh2000@gmail.com>
2020-07-23 02:34:03 +0530

    mid work

--- app1/models.py
+++ app1/models.py
@@ -12,7 +12,6 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    url = models.SlugField(max_length=100, unique=False, blank=True, editable=False)
     image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True, null=True)
@@ -68,3 +67,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return 'Comment {} by {}'.format(self.body, self.name)
+
+class Going(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='goings')
+    name = models.CharField(max_length=80)
+    going= models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+
+    
+
+    def __str__(self):
+        return ''.format(self.name)
